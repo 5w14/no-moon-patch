ecdc94134a24d1d4fcf56c800028f262895106a9
5w14 <max@maxthetomas.ru>
2025-10-20 21:01:22 +0400

    neoforge -> forge

--- src/main/java/com/fivew14/nomoonpatch/Nomoonpatch.java
+++ src/main/java/com/fivew14/nomoonpatch/Nomoonpatch.java
@@ -1,14 +1,11 @@
 package com.fivew14.nomoonpatch;
 
-import net.minecraft.util.RandomSource;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(Nomoonpatch.MODID)
 public class Nomoonpatch {
     public static final float PACKET_DROP_CHANCE = 7f/8f;
     public static final String MODID = "no_moon_network_patch";
-    public Nomoonpatch(IEventBus modEventBus, ModContainer modContainer) {
+    public Nomoonpatch() {
     }
 }
