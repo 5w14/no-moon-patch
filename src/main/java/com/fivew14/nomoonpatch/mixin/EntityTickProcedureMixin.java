cf1cdcd04d0f1b4a2761440e2c937362dca063b3
5w14 <max@maxthetomas.ru>
2025-10-28 01:08:22 +0400

    chore: bump version to 0.0.5

--- src/main/java/com/fivew14/nomoonpatch/mixin/EntityTickProcedureMixin.java
+++ src/main/java/com/fivew14/nomoonpatch/mixin/EntityTickProcedureMixin.java
@@ -14,7 +14,8 @@ import java.util.function.Predicate;
 
 @Mixin(EntityTickProcedure.class)
 public class EntityTickProcedureMixin {
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"), method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/level/LevelAccessor;DDDLnet/minecraft/world/entity/Entity;)V")
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"),
+            method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/level/LevelAccessor;DDDLnet/minecraft/world/entity/Entity;)V")
     private static <T extends Entity> List<T> removeLookupEveryTick(LevelAccessor instance, Class<T> aClass, AABB aabb, Predicate<T> predicate) {
         if (Nomoonpatch.entityMixinSkipExpensiveCalls)
             return List.of();
